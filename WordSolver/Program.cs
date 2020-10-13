8893e51133ff564ad7af117eab892da53761d5ed
derickard <derickard@gmail.com>
2020-10-13 09:17:16 -0400

    Add project files.

---
+++ WordSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WordSolver/Solution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSolver
+{
+    class Solution
+    {
+        public List<string> words { get; set; }
+
+        
+    }
+}
